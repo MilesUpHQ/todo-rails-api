a98f5a956d381f11f3ad8b04d07f3c2947a0a762
Magesh <magesh@sedintechnologies.com>
2019-12-18 23:20:15 +0530

    bumped version of few gems and updated Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181116071718) do
+ActiveRecord::Schema.define(version: 2018_11_16_071718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "todos", force: :cascade do |t|
-    t.string   "title"
+  create_table "todos", id: :serial, force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
